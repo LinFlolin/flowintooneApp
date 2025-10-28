1121733395f4e61810a6f8a7e7e3f5b63912491d
Datchani <d.imnoy@tlco.it>
2025-10-28 15:57:27 +0100

    add tailwind and the darck theme

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,8 +5,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-     <Text>
-         home
+     <Text className=' text-white text-2xl self-center '>
+         Welcome to the Home page!
      </Text>
    
   );
